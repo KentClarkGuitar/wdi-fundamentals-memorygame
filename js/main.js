6838ce7b6f5b5c8f6994bdb6f166b18243f8b7a6
Kent <kent@Kents-MacBook-Pro.local>
2018-06-05 21:18:12 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne;
+var cardTwo;
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+}
+
+flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+checkForMatch();
+}
+flipCard(0);
+flipCard(2)
+
+
